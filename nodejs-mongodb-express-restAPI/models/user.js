b8a420231c2dfd75e1a2efdd4c7e41497681a13f
X257488 <X257488@one.merckgroup.com>
2024-02-21 13:49:53 +0530

    added code to encrypt password using bcryptjs and middleware

--- nodejs-mongodb-express-restAPI/models/user.js
+++ nodejs-mongodb-express-restAPI/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -29,6 +29,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
